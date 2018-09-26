2ad76f2d74ecf0d94839aaa217b6f2f6b6d9aa5b
Odin Ugedal <odin@ugedal.com>
2018-09-27 01:39:12 +0200

    Add filtering and 'latestReadme'

--- handler.go
+++ handler.go
@@ -5,6 +5,9 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"sort"
+	"strconv"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 	graphql "github.com/graph-gophers/graphql-go"
@@ -18,13 +21,15 @@ schema {
 		query: Query
 }
 type Query {
-	readmeUtgaver(year: Int): [ReadmeUtgave!]!
+	readmeUtgaver(year: Int utgave: Int): [ReadmeUtgave!]!
+	latestReadme: ReadmeUtgave
 }
 type ReadmeUtgave {
 	title: String!
 	image: String!
 	pdf: String!
 	year: Int!
+	utgave: Int!
 }
 
 `
@@ -32,10 +37,11 @@ const url = "https://readme.abakus.no/"
 
 // ReadmeUtgave is a cool struct
 type ReadmeUtgave struct {
-	Title string
-	Image string
-	Pdf   string
-	Year  int32
+	Title  string
+	Image  string
+	Pdf    string
+	Year   int32
+	Utgave int32
 }
 
 func (r ReadmeUtgave) TITLE() string {
@@ -50,8 +56,40 @@ func (r ReadmeUtgave) IMAGE() string {
 func (r ReadmeUtgave) YEAR() int32 {
 	return r.Year
 }
+func (r ReadmeUtgave) UTGAVE() int32 {
+	return r.Utgave
+}
+func (q *query) LatestReadme(ctx context.Context) *ReadmeUtgave {
+	rs := getSortedReadmes()
+	if len(rs) == 0 {
+		return nil
+	}
+	return &rs[0]
+}
 
-func (q *query) ReadmeUtgaver(ctx context.Context, input *struct{ Year *int32 }) []ReadmeUtgave {
+func (q *query) ReadmeUtgaver(ctx context.Context, input *struct {
+	Year   *int32
+	Utgave *int32
+}) []ReadmeUtgave {
+	readmes := getSortedReadmes()
+	if input == nil {
+		return readmes
+	}
+	filteredReadmes := readmes[:0]
+	for _, r := range readmes {
+		if input.Year != nil && *input.Year != r.Year {
+			continue
+		}
+		if input.Utgave != nil && *input.Utgave != r.Utgave {
+			continue
+		}
+		filteredReadmes = append(filteredReadmes, r)
+	}
+
+	return filteredReadmes
+}
+
+func getSortedReadmes() []ReadmeUtgave {
 	// Request the HTML page.
 	res, err := http.Get(url)
 	if err != nil {
@@ -74,19 +112,27 @@ func (q *query) ReadmeUtgaver(ctx context.Context, input *struct{ Year *int32 })
 		title, _ := s.Find("img").Attr("alt")
 		image, _ := s.Find("img").Attr("src")
 		pdf, _ := s.Find("a").Attr("href")
-		year := int32(0)
+		splitted := strings.Split(title, " ")
+		year, _ := strconv.Atoi(splitted[len(splitted)-1])
+		utgaveNr, _ := strconv.Atoi(splitted[len(splitted)-2])
 
 		utgave := ReadmeUtgave{
-			Year:  year,
-			Title: title,
-			Pdf:   pdf,
-			Image: image,
+			Year:   int32(year),
+			Title:  title,
+			Pdf:    pdf,
+			Image:  image,
+			Utgave: int32(utgaveNr),
 		}
 		utgaver = append(utgaver, utgave)
 	})
+	sort.Slice(utgaver, func(i, j int) bool {
+		if utgaver[i].Year == utgaver[j].Year {
+			return utgaver[i].Utgave > utgaver[j].Utgave
+		}
+		return utgaver[i].Year > utgaver[j].Year
+	})
 
 	return utgaver
-
 }
 
 var params struct {
